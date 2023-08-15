ea9937379ba0c2d1d8d28a4fd1733886a53aaed2
Anwar <anwarrehman77@gmail.com>
2023-08-15 07:38:00 -0700

    Login Page

---
+++ backend/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://anwarrehman77:sKHlvAIDmx2vMPfo@betterbreakfast.fpnwgkf.mongodb.net/Test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const User = mongoose.model('User', {
+  username: String,
+  password: String,
+});
+
+const BreakfastOption = mongoose.model('BreakfastOption', {
+    name: String,
+    healthinessScore: Number
+})
+
+// User API's
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+      const existingUser = await User.findOne({ username });
+  
+      if (existingUser) {
+        res.json({ success: false, message: 'Username already taken' });
+      } else {
+        const newUser = new User({ username, password });
+        await newUser.save();
+        res.json({ success: true, message: 'Registration successful' });
+      }
+    } catch (error) {
+      res.status(500).json({ success: false, message: 'An error occurred' });
+    }
+  });
+  
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username, password });
+
+  if (user) {
+    res.json({ success: true, message: 'Login successful' });
+  } else {
+    res.json({ success: false, message: 'Invalid credentials' });
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String _message = '';
+
+  Future<void> _login() async {
+    debugPrint("Login Button");
+    final response = await http.post(
+      Uri.parse('http://localhost:8080/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': _usernameController.text,
+        'password': _passwordController.text,
+      }),
+    );
+
+    final data = jsonDecode(response.body);
+    setState(() {
+      _message = data['message'];
+    });
+
+    if (data['success']) {
+      String textToSend = _usernameController.text;
+      // ignore: use_build_context_synchronously
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Dashboard(
+              text: textToSend,
+              key: Key("Key"),
+            ),
+          ));
+    }
+  }
+
+  Future<void> _register() async {
+    debugPrint("Register Button");
+    final response = await http.post(
+      Uri.parse('http://localhost:8080/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': _usernameController.text,
+        'password': _passwordController.text,
+      }),
+    );
+
+    final data = jsonDecode(response.body);
+    setState(() {
+      _message = data['message'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _register,
+              child: const Text('Register'),
+            ),
+            const SizedBox(height: 10),
+            Text(_message),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  final String text;
+
+  // receive data from the FirstScreen as a parameter
+  const Dashboard({required Key key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second screen')),
+      body: Center(
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
